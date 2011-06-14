c4f489aec4462990101b488d4191ca2dcabd8d68
singmann <singmann@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-14 17:19:36 +0000

    the test files now includes tests for a reference result of Bröder & Schütz (2009, Experiment 3). Furthermore, testing is nicer formatted.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mptinr@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- source_MPTinR.R
+++ source_MPTinR.R
@@ -2,3 +2,51 @@
 for (r.file in list.files("pkg/R", full.names = TRUE)) {
 	source(r.file)
 }
+
+
+# Source testfunctions
+source("test_MPTinR.R")
+numerical.accuracy <- 1e-8
+
+################################################################
+## Test 1: Test fitting against references in the literature: ##
+################################################################
+
+# Example 1 (?fit.mpt): Proactive Inhibiton Model from Riefer & Batchelder (1988):
+# The parameter values for proactive inhibition model are tested against reference (Riefer & Batchelder,1988, Table 1)
+# Maximal accepted difference in parameter values is <= .01
+suppressWarnings(example1(.01, numerical.accuracy))
+
+# Example 2 (?fit.mpt): Storage-Retrieval Model from Riefer & Batchelder (1988):
+# This tests compares the likelihood ratios tests for two restricted models (setting either r or c equal)
+# with the reference in Batchelder & Riefer (1988, p. 332)
+suppressWarnings(example2(numerical.accuracy))
+
+# Example 3 (?fit.mpt): 2HTM Recognition Model with Data from Bröder & Schütz (2009, Experiment 3)
+# This tests compares the G² values and the parameter values from Broeder & Schütz (2009, Experiment 3)
+# with the reference in their paper (Table 4)
+example.broeder.ex3(0.01, numerical.accuracy)
+
+
+#######################################################################################
+## Test 2: Test fitting of multi-individual fit against reference result from MPTinR ##
+#######################################################################################
+
+# current reference was calculated with MPTinR 0.6.3
+
+load("testfiles/bs_ref_063.RData")
+
+example.broeder.mptinr(5)
+
+all.equal(br.2htm.ref[-6], br.2htm.test[-6], tolerance = 0.01)
+all.equal(br.2htm.ineq.ref[-6], br.2htm.ineq.test[-6], tolerance = 0.01)
+
+all.equal(br.2htm.res.ref[-6], br.2htm.res.test[-6], tolerance = 0.001)
+all.equal(br.2htm.res.ineq.ref[-6], br.2htm.res.ineq.test[-6], tolerance = 0.001)
+
+all.equal(br.1htm.ref[-6], br.1htm.test[-6], tolerance = 0.001)
+all.equal(br.1htm.ineq.ref[-6], br.1htm.ineq.test[-6], tolerance = 0.001)
+
+
+
+
--- test_MPTinR.R
+++ test_MPTinR.R
@@ -1,65 +1,52 @@
 
-numerical.accuracy <- 1e-8
-
-# Check Example 1 (?fit.mpt) against data in Paper:
-example1 <- function(max.diff) {
-    load("pkg/data/rb.fig1.data.rda")
-    model1 <- "pkg/inst/extdata/rb.fig1.model"
-    model1.eqn <- "pkg/inst/extdata/rb.fig1.model.eqn"
-
-    # just fit the first "individual":
-    rb.table1.res <- fit.mpt(rb.fig1.data, model1, n.optim = 1)
-
-    load("testfiles/rb.table1.RData")
-
-    e1.estimate <- round(t(rb.table1.res$parameters$individual[,1,]),2)
-
-    (max(abs(rb.table1 - e1.estimate)) - max.diff) < numerical.accuracy
+# Example 1 (?fit.mpt): Proactive Inhibiton Model from Riefer & Batchelder (1988):
+example1 <- function(max.diff, numerical.accuracy) {
+	load("pkg/data/rb.fig1.data.rda")
+	model1 <- "pkg/inst/extdata/rb.fig1.model"
+	model1.eqn <- "pkg/inst/extdata/rb.fig1.model.eqn"
+	rb.table1.res <- fit.mpt(rb.fig1.data, model1, n.optim = 1)
+	load("testfiles/rb.table1.RData")
+	e1.estimate <- round(t(rb.table1.res$parameters$individual[,1,]),2)
+	(max(abs(rb.table1 - e1.estimate)) - max.diff) < numerical.accuracy
 }
 
-# Check parameter values for proactive inhibition model against reference (Riefer & Batchelder,1988, Table 1)
-# Maximal accepted difference in parameter values is <= .01
-example1(.01)
-
-# Example 2: Stirage-Retrieval Model from Reifer & batchelder (1988)
-
-example2 <- function() {
-load("pkg/data/rb.fig2.data.rda")
-
-model2 <- "pkg/inst/extdata/rb.fig2.model"
-model2r.r.eq <- "pkg/inst/extdata/rb.fig2.r.equal"
-model2r.c.eq <- "pkg/inst/extdata/rb.fig2.c.equal"
-
-# The full (i.e., unconstrained) model
-ref.model <- fit.mpt(rb.fig2.data, model2, n.optim = 10)
-
-# All r equal
-r.equal <- fit.mpt(rb.fig2.data, model2, model2r.r.eq, n.optim = 10)
-
-c.equal <- fit.mpt(rb.fig2.data, model2, model2r.c.eq, n.optim = 10)
-
-test.r <- round((r.equal[["goodness.of.fit"]][["G.Squared"]] - ref.model[["goodness.of.fit"]][["G.Squared"]]), 2) - 35.81 < numerical.accuracy
-
-test.c <- round((g.sq.c.equal <- c.equal[["goodness.of.fit"]][["G.Squared"]] - ref.model[["goodness.of.fit"]][["G.Squared"]]),2) - 1.43  < numerical.accuracy
-list(test.for.r = test.r, test.for.c = test.c)
+# Example 2 (?fit.mpt): Storage-Retrieval Model from Riefer & Batchelder (1988):
+example2 <- function(numerical.accuracy) {
+	load("pkg/data/rb.fig2.data.rda")
+	model2 <- "pkg/inst/extdata/rb.fig2.model"
+	model2r.r.eq <- "pkg/inst/extdata/rb.fig2.r.equal"
+	model2r.c.eq <- "pkg/inst/extdata/rb.fig2.c.equal"
+	ref.model <- fit.mpt(rb.fig2.data, model2, n.optim = 10)
+	r.equal <- fit.mpt(rb.fig2.data, model2, model2r.r.eq, n.optim = 10)
+	c.equal <- fit.mpt(rb.fig2.data, model2, model2r.c.eq, n.optim = 10)
+	test.r <- round((r.equal[["goodness.of.fit"]][["G.Squared"]] - ref.model[["goodness.of.fit"]][["G.Squared"]]), 2) - 35.81 < numerical.accuracy
+	test.c <- round((g.sq.c.equal <- c.equal[["goodness.of.fit"]][["G.Squared"]] - ref.model[["goodness.of.fit"]][["G.Squared"]]),2) - 1.43  < numerical.accuracy
+	list(test.for.r = test.r, test.for.c = test.c)
 }
 
-# This tests compares the likelihood ratios tests for two restricted models (setting either r or c equal)
-# with the reference in Batchelder & Riefer (1988, p. 332)
-
-suppressWarnings(suppressMessages(example2()))
-
+# Example 3 (?fit.mpt): 2HTM Recognition Model with Data from Bröder & Schütz (2009, Experiment 3)
 example.broeder.ex3 <- function(max.diff, numerical.accuracy) {
-
-    load("pkg/data/d.broeder.rda")
-    m.2htm <- "pkg/inst/extdata/5points.2htm.model"
-    br.2htm <- fit.mpt(apply(d.broeder,2,sum), m.2htm, n.optim = 5)
-    g2 <- (br.2htm$goodness.of.fit[["G.Squared"]] - 2.83 - max.diff) < numerical.accuracy
-    ps <- (max(abs(br.2htm$parameters[,1] - c(.45, .56, .14, .22, .40, .60, .69))) - max.diff) < numerical.accuracy
-    list(goodness.of.fit = g2, parameters = ps)
+	load("pkg/data/d.broeder.rda")
+	m.2htm <- "pkg/inst/extdata/5points.2htm.model"
+	br.2htm <- fit.mpt(apply(d.broeder,2,sum), m.2htm, n.optim = 5)
+	g2 <- (br.2htm$goodness.of.fit[["G.Squared"]] - 2.83 - max.diff) < numerical.accuracy
+	ps <- (max(abs(br.2htm$parameters[,1] - c(.45, .56, .14, .22, .40, .60, .69))) - max.diff) < numerical.accuracy
+	list(goodness.of.fit = g2, parameters = ps)
 }
 
-# This examples compares the data from Broeder & Schütz (2009, Experiment 3) with the reference (Table 4)
-example.broeder.ex3(0.01, numerical.accuracy)
-
+example.broeder.mptinr <- function(n.optim) {
+	load("pkg/data/d.broeder.rda")
+	m.2htm <- "pkg/inst/extdata/5points.2htm.model"
+	r.2htm <- "pkg/inst/extdata/broeder.2htm.restr"
+	r.1htm <- "pkg/inst/extdata/broeder.1htm.restr"
+	i.2htm <- "pkg/inst/extdata/broeder.2htm.ineq"
+	ir.2htm <- "pkg/inst/extdata/broeder.2htm.restr.ineq"
+	ir.1htm <- "pkg/inst/extdata/broeder.1htm.restr.ineq"
+	assign("br.2htm.test", fit.mpt(d.broeder, m.2htm, n.optim = n.optim), envir = .GlobalEnv)
+	assign("br.2htm.ineq.test", fit.mpt(d.broeder, m.2htm, i.2htm, n.optim = n.optim), envir = .GlobalEnv)
+	assign("br.2htm.res.test", fit.mpt(d.broeder, m.2htm, r.2htm, n.optim = n.optim), envir = .GlobalEnv)
+	assign("br.2htm.res.ineq.test", fit.mpt(d.broeder, m.2htm, ir.2htm, n.optim = n.optim), envir = .GlobalEnv)
+	assign("br.1htm.test", fit.mpt(d.broeder, m.2htm, r.1htm, n.optim = n.optim), envir = .GlobalEnv)
+	assign("br.1htm.ineq.test", fit.mpt(d.broeder, m.2htm, ir.1htm, n.optim = n.optim), envir = .GlobalEnv)
+}
 
